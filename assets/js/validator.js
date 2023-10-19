3ede91369e82c091879efba44313ee05e77c8d72
Hữu Phúc <20521770@gm.uit.edu.vn>
2023-10-20 06:03:48 +0700

    sign-up & form validator

---
+++ assets/js/validator.js
@@ -0,0 +1,108 @@
+// Handle Form Validation
+function Validator(options) {
+    var selectorRules = {};
+
+    // Excute Validate
+    function validate(inputElement, rule) {
+        var errorElement = inputElement.parentElement.parentElement.querySelector(options.errorSelector);
+        var errorMessage;
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rules và check
+        // Nếu có lỗi thì dừng
+        for (var i = 0; i < rules.length; i++) {
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add("invalid");
+        } else {
+            errorElement.innerText = "";
+            inputElement.parentElement.classList.remove("invalid");
+        }
+    }
+
+    // Get element of form for validate
+    var formElement = document.querySelector(options.form);
+
+    if (formElement) {
+        // Khi submit
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+        };
+
+        // Xử lý lặp qua mỗi rules và handle event
+        options.rules.forEach((rule) => {
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
+            var inputElement = formElement.querySelector(rule.selector);
+
+            if (inputElement) {
+                // Handle blur cases from input tags
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                };
+                // Handled when user enters
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    errorElement.innerText = "";
+                    inputElement.parentElement.classList.remove("invalid");
+                };
+            }
+        });
+    }
+}
+
+// Define Rules
+// Nguyên tắc:
+// 1. Có lỗi => Trả message lỗi
+// 2. Không có => Không trả gì cả (Undefined)
+
+Validator.isRequired = function (selector, message) {
+    return {
+        selector,
+        test: function (value) {
+            return value.trim() ? undefined : message || "Vui lòng nhập trường này!";
+        },
+    };
+};
+
+Validator.isEmail = function (selector, message) {
+    return {
+        selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+            return regex.test(value) ? undefined : message || "Vui lòng nhập email";
+        },
+    };
+};
+
+Validator.minLength = function (selector, min, message) {
+    return {
+        selector,
+        test: function (value) {
+            return value.length >= min ? undefined : message || `Vui lòng nhập vào tối thiểu ${min} kí tự`;
+        },
+    };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector,
+        test: function (value) {
+            return value === getConfirmValue()
+                ? undefined
+                : message || "Mật khẩu không trùng khớp. Xin vui lòng nhập lại!";
+        },
+    };
+};
